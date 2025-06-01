b3d1e7867654c34c2394424d4a37aab75cd6d2a6
GabrielAlbq20 <gabriel.albqdev@outlook.com>
2025-05-31 23:30:54 -0300

    corrigindo erros de tipagem e linha longa no settings.py

--- project/settings.py
+++ project/settings.py
@@ -8,12 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fxe+@vvuooi8v!5v83ghl5@)e+7un06!ek6(vi!%bvx-lm)j=@'
+SECRET_KEY = 'django-insecure-fxe+@vvuooi8v!5v83ghl5@)e+7un06!ek6(vi!%bvx-lm)j=@' # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
@@ -79,17 +79,17 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa: E501
+    }, 
 ]
 
 
